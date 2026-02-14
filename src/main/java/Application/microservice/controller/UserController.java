b361d6458d9a50e106a718eef146fde291f2cc36
AlekseiTsukanov <willmord2@gmail.com>
2026-02-14 17:07:52 +0500

    Task-3.1.1

---
+++ src/main/java/Application/microservice/controller/UserController.java
@@ -0,0 +1,65 @@
+package Application.microservice.controller;
+
+import Application.microservice.model.User;
+import Application.microservice.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String users(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "users";
+    }
+
+    @PostMapping
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id) {
+        userService.deleteById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/show/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("users", userService.findById(id));
+        return "users";
+    }
+
+    @GetMapping("/create")
+    public String createUser(Model model) {
+        model.addAttribute("user", new User());
+        return "users-constructor";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "users-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/";
+    }
+}
